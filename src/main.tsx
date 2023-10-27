85d5b719eac2cfef5d4c413bc06326721d9b8e16
marczerwinski <marczerwinski@gmail.com>
2023-10-27 16:32:56 +0200

    update types for entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Test from "./Test";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.querySelector('#root') as HTMLElement);
 root.render(<Test />)
